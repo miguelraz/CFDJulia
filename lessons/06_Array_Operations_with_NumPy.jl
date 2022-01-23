caf3a27c0a1e5c1199a848ebad0d3544ae81fc72
Miguel Raz Guzmán Macedo <miguelraz@gmail.com>
2022-01-23 10:49:57 -0600

    06_Array_Operations_with_Julia.jl

--- lessons/06_Array_Operations_with_NumPy.jl
+++ lessons/06_Array_Operations_with_NumPy.jl
@@ -1,232 +1,170 @@
-Text provided under a Creative Commons Attribution license, CC-BY.  All code is made available under the FSF-approved BSD-3 license.  (c) Lorena A. Barba, Gilbert F. Forsyth 2017. Thanks to NSF for support via CAREER award #1149784.
-[@LorenaABarba](https://twitter.com/LorenaABarba)
+# Text provided under a Creative Commons Attribution license, CC-BY.  All code is made available under the FSF-approved BSD-3 license.  (c) Lorena A. Barba, Gilbert F. Forsyth 2017. Thanks to NSF for support via CAREER award #1149784.
+# [@LorenaABarba](https://twitter.com/LorenaABarba)
+# Translation to Julia by [@miguelraz_](https://twitter.com/miguelraz_)
 
-12 steps to Navier–Stokes
-=====
-***
+## 12 steps to Navier–Stokes
+# =====
+# ***
 
-This lesson complements the first interactive module of the online [CFD Python](https://github.com/barbagroup/CFDPython) class, by Prof. Lorena A. Barba, called **12 Steps to Navier–Stokes.** It was written with BU graduate student Gilbert Forsyth.
+# This lesson complements the first interactive module of the online [CFD Python](https://github.com/barbagroup/CFDPython) class, by Prof. Lorena A. Barba, called **12 Steps to Navier–Stokes.** It was written with BU graduate student Gilbert Forsyth.
 
-Array Operations with NumPy
-----------------
+## Array Operations with NumPy
+# ----------------
 
-For more computationally intensive programs, the use of built-in Numpy functions can provide an  increase in execution speed many-times over.  As a simple example, consider the following equation:
+# For more computationally intensive programs, the use of built-in Numpy functions can provide an  increase in execution speed many-times over.  As a simple example, consider the following equation:
 
-$$u^{n+1}_i = u^n_i-u^n_{i-1}$$
+# $$u^{n+1}_i = u^n_i-u^n_{i-1}$$
 
-Now, given a vector $u^n = [0, 1, 2, 3, 4, 5]\ \ $   we can calculate the values of $u^{n+1}$ by iterating over the values of $u^n$ with a for loop.  
-
-
-```python
-import numpy
-```
+# Now, given a vector $u^n = [0, 1, 2, 3, 4, 5]\ \ $   we can calculate the values of $u^{n+1}$ by iterating over the values of $u^n$ with a for loop.
 
 
-```python
-u = numpy.array((0, 1, 2, 3, 4, 5))
+u = [0, 1, 2, 3, 4, 5]
 
-for i in range(1, len(u)):
-    print(u[i] - u[i-1])
-```
-
-    1
-    1
-    1
-    1
-    1
+for i in 1:length(u)
+    println(u[i] - u[i-1])
+end
 
 
-This is the expected result and the execution time was nearly instantaneous.  If we perform the same operation as an array operation, then rather than calculate $u^n_i-u^n_{i-1}\ $ 5 separate times, we can slice the $u$ array and calculate each operation with one command:
+# This is the expected result and the execution time was nearly instantaneous.
+# We can calculate each operation with one function:
 
+diff(u)
 
-```python
-u[1:] - u[0:-1]
-```
-
-
-
-
-    array([1, 1, 1, 1, 1])
-
-
-
-What this command says is subtract the 0th, 1st, 2nd, 3rd, 4th and 5th elements of $u$ from the 1st, 2nd, 3rd, 4th, 5th and 6th elements of $u$.  
+# What this command says is subtract the 1st, 2nd, 3rd, 4th and 5th elements of $u$ from the 2nd, 3rd, 4th, 5th and 6th elements of $u$.
 
 ### Speed Increases
+# In NumPy, we would have to learn a bucketful about how to restructure our program to exploit "vectorization".
+# This unfortunately makes code very brittle and diminishes readability, but it's require by NumPy for speed.
+# We have no such obstacles in Julia - if you write simple for loops, they will start out being "fast enough",
+# and then we can move on to talk about worthy optimizations later.
+# Note: There
 
-For a 6 element array, the benefits of array operations are pretty slim.  There will be no appreciable difference in execution time because there are so few operations taking place.  But if we revisit 2D linear convection, we can see some substantial speed increases.  
 
-
-
-```python
-nx = 81
-ny = 81
-nt = 100
+nₓ = 81
+ny = 81 # note we can't call this n\_y because of the Unicode standard, not because of Julia
+nₜ = 100
 c = 1
-dx = 2 / (nx - 1)
-dy = 2 / (ny - 1)
-sigma = .2
-dt = sigma * dx
+Δx = 2 / (nx - 1)
+Δy = 2 / (ny - 1)
+σ = 0.2
+Δt = σ * Δx
 
-x = numpy.linspace(0, 2, nx)
-y = numpy.linspace(0, 2, ny)
+x = range(start = 0, stop = 2, length = nₓ)
+y = range(start = 0, stop = 2, length = ny)
 
-u = numpy.ones((ny, nx)) ##create a 1xn vector of 1's
-un = numpy.ones((ny, nx)) 
+### TODO
+u = ones(ny, nₓ) ##create a 1xn vector of 1's
+uₙ = ones(ny, nₓ)
 
 ###Assign initial conditions
+#We can try the following to start the initial conditions:
+# TODO - cartesian indexes
+rangey = (.5/Δy):(1/Δy)
+rangex = (.5/Δx):(1/Δx)
+for (_, j) in rangey
+    for (_, i) in rangex
+        u[i, j] = 2.0
+    end
+end
+u
 
-u[int(.5 / dy): int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
-```
+# But we could also try doing this with `CartesianIndexes` - they will subset a rectangular region of our array (for any dimension!)
+# and loop efficiently through it. You can find more about them if you press `?CartesianIndex` in the REPL
+#u[int(.5 / dy): int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
 
-With our initial conditions all set up, let's first try running our original nested loop code, making use of the iPython "magic" function `%%timeit`, which will help us evaluate the performance of our code. 
+u[CartesianIndices((rangex,rangey))] .= 2
 
-**Note**: The `%%timeit` magic function will run the code several times and then give an average execution time as a result.  If you have any figures being plotted within a cell where you run `%%timeit`, it will plot those figures repeatedly which can be a bit messy. 
+# With our initial conditions all set up, let's first try running our original nested loop code, making use of the
+# BenchmarkTools.jl `@btime` macro, which will help us evaluate the performance of our code.
 
-The execution times below will vary from machine to machine.  Don't expect your times to match these times, but you _should_ expect to see the same general trend in decreasing execution time as we switch to array operations.
+# **Note**: The `@btime` macro will run the code several times and then give an average execution time as a result.
+# If you have any figures being plotted within a code cell where you run `@btime`,
+# it will plot those figures repeatedly which can be a bit messy and slow.
 
+# The execution times below will vary from machine to machine.  Don't expect your times to match these times,
+# but you _should_ expect to see the same general trend in decreasing execution time as we switch to array operations.
 
-```python
-%%timeit
-u = numpy.ones((ny, nx))
-u[int(.5 / dy): int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
+using BenchmarkTools
 
-for n in range(nt + 1): ##loop across number of time steps
-    un = u.copy()
-    row, col = u.shape
-    for j in range(1, row):
-        for i in range(1, col):
-            u[j, i] = (un[j, i] - (c * dt / dx * 
-                                  (un[j, i] - un[j, i - 1])) - 
-                                  (c * dt / dy * 
-                                   (un[j, i] - un[j - 1, i])))
-            u[0, :] = 1
-            u[-1, :] = 1
-            u[:, 0] = 1
-            u[:, -1] = 1
-```
-
-    3.07 s ± 15.1 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
-
-
-With the "raw" Python code above, the mean execution time achieved was 3.07 seconds (on a MacBook Pro Mid 2012).  Keep in mind that with these three nested loops, that the statements inside the **j** loop are being evaluated more than 650,000 times.   Let's compare that with the performance of the same code implemented with array operations:
+# TODO
+@btime u[int(.5 / dy): int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2 :setup=(u = ones(ny, nx))
 
 
-```python
-%%timeit
-u = numpy.ones((ny, nx))
-u[int(.5 / dy): int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
+@btime begin
+    for n in 1:nₜ ##loop across number of time steps
+    uₙ = copy(u)
+    row, col = size(u)
+    for j in 1:row
+        for i in 1:col
+            u[j, i] = (uₙ[j, i] - (c * Δt / Δx *
+                                  (uₙ[j, i] - uₙ[j, i - 1])) -
+                                  (c * Δt / Δy *
+                                   (uₙ[j, i] - uₙ[j - 1, i])))
+            u[1, :] = 1.0
+            u[end, :] = 1.0
+            u[:, 1] = 1.0
+            u[:, end] = 1.0
+        end
+    end
+    end
+end :setup=(u = ones(ny, nx))
 
-for n in range(nt + 1): ##loop across number of time steps
-    un = u.copy()
-    u[1:, 1:] = (un[1:, 1:] - (c * dt / dx * (un[1:, 1:] - un[1:, 0:-1])) -
-                              (c * dt / dy * (un[1:, 1:] - un[0:-1, 1:])))
-    u[0, :] = 1
-    u[-1, :] = 1
-    u[:, 0] = 1
-    u[:, -1] = 1
-```
-
-    7.38 ms ± 105 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
-
-
-As you can see, the speed increase is substantial.  The same calculation goes from 3.07 seconds to 7.38 milliseconds.  3 seconds isn't a huge amount of time to wait, but these speed gains will increase exponentially with the size and complexity of the problem being evaluated.  
-
-
-```python
-from IPython.core.display import HTML
-def css_styling():
-    styles = open("../styles/custom.css", "r").read()
-    return HTML(styles)
-css_styling()
-```
+## Python comparison
+# With the "raw" Python code above, the mean execution time achieved was 3.07 seconds (on a MacBook Pro Mid 2012).
+# Keep in mind that with these three nested loops, that the statements inside the **j** loop are being evaluated more than 650,000 times.
+# Let's compare that with the performance of the same code implemented with array operations:
+# I've left the vectorized version so that people can compare the code and **appreciate** how substantial the difference from Julia is.
 
 
+# ```python
+# %%timeit
+# u = numpy.ones((ny, nx))
+# u[int(.5 / dy): int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
+
+# for n in range(nt + 1): ##loop across number of time steps
+#     un = u.copy()
+#     u[1:, 1:] = (un[1:, 1:] - (c * dt / dx * (un[1:, 1:] - un[1:, 0:-1])) -
+#                               (c * dt / dy * (un[1:, 1:] - un[0:-1, 1:])))
+#     u[0, :] = 1
+#     u[-1, :] = 1
+#     u[:, 0] = 1
+#     u[:, -1] = 1
+# ```
+
+#     7.38 ms ± 105 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
 
 
-<link href='http://fonts.googleapis.com/css?family=Fenix' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Alegreya+Sans:100,300,400,500,700,800,900,100italic,300italic,400italic,500italic,700italic,800italic,900italic' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro:300,400' rel='stylesheet' type='text/css'>
-<style>
-    @font-face {
-        font-family: "Computer Modern";
-        src: url('http://mirrors.ctan.org/fonts/cm-unicode/fonts/otf/cmunss.otf');
-    }
-    div.cell{
-        width:800px;
-        margin-left:16% !important;
-        margin-right:auto;
-    }
-    h1 {
-        font-family: 'Alegreya Sans', sans-serif;
-    }
-    h2 {
-        font-family: 'Fenix', serif;
-    }
-    h3{
-		font-family: 'Fenix', serif;
-        margin-top:12px;
-        margin-bottom: 3px;
-       }
-	h4{
-		font-family: 'Fenix', serif;
-       }
-    h5 {
-        font-family: 'Alegreya Sans', sans-serif;
-    }	   
-    div.text_cell_render{
-        font-family: 'Alegreya Sans',Computer Modern, "Helvetica Neue", Arial, Helvetica, Geneva, sans-serif;
-        line-height: 135%;
-        font-size: 120%;
-        width:600px;
-        margin-left:auto;
-        margin-right:auto;
-    }
-    .CodeMirror{
-            font-family: "Source Code Pro";
-			font-size: 90%;
-    }
-/*    .prompt{
-        display: None;
-    }*/
-    .text_cell_render h1 {
-        font-weight: 200;
-        font-size: 50pt;
-		line-height: 100%;
-        color:#CD2305;
-        margin-bottom: 0.5em;
-        margin-top: 0.5em;
-        display: block;
-    }	
-    .text_cell_render h5 {
-        font-weight: 300;
-        font-size: 16pt;
-        color: #CD2305;
-        font-style: italic;
-        margin-bottom: .5em;
-        margin-top: 0.5em;
-        display: block;
-    }
+# As you can see, the speed increase is substantial in Python.  The same calculation goes from 3.07 seconds to 7.38 milliseconds.
+# 3 seconds isn't a huge amount of time to wait, but these speed gains will increase exponentially
+# with the size and complexity of the problem being evaluated.
 
-    .warning{
-        color: rgb( 240, 20, 20 )
-        }  
-</style>
-<script>
-    MathJax.Hub.Config({
-                        TeX: {
-                           extensions: ["AMSmath.js"]
-                           },
-                tex2jax: {
-                    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-                    displayMath: [ ['$$','$$'], ["\\[","\\]"] ]
-                },
-                displayAlign: 'center', // Change this to 'center' to center equations.
-                "HTML-CSS": {
-                    styles: {'.MathJax_Display': {"margin": 4}}
-                }
-        });
-</script>
+## Julia timings:
+# We're going to be doing some mild modifications to the code, which will involve
+# - avoid global variables (even though Julia 1.8 will not penalize this), as it's good practice
+# - diminishing allocations (because [views in Julia copy by default](TODO perf manual and views))
+# - putting code inside of a function
+# There's very little point to write efficient array code if it's not within a function in Julia.
+# This is because Julia needs to JIT compile your code to get all the speedups.
+# The JIT compiler does not need you to use type hints or annotations to work properly, it's only necessary that
+# [TODO type stability]() function output types be predictable on input types only.
+#
+function time1(u, params)
+    u = ones(ny, nₓ)
+    (;Δt, Δx, c, nₜ) = params
 
-
-
+    for n in 1:nₜ ##loop across number of time steps
+    uₙ = copy(u)
+            @views u[:,:] .= (uₙ[2:end, 2:end] - (c * Δt / Δx *
+                                  (uₙ[2:end, 2:end] - uₙ[2:end, 1:end])) -
+                                  (c * Δt / Δy *
+                                   (uₙ[2:end, 2:end] - uₙ[1:end, 2:end])))
+            @views u[1, :] = 1.0
+            @views u[end, :] = 1.0
+            @views u[:, 1] = 1.0
+            @views u[:, end] = 1.0
+        end
+    end
+    end
+end
+params = (;Δt, Δx, c, nₜ)
+@btime time1(u, params)
